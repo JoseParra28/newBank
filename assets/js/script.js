433d7c3acf3f3e58f123dd03f64cc51f442e3b53
JoseParra28 <iliftandcook@gmail.com>
2023-06-25 16:29:16 +0000

    dataset click events

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
-console.log("holet he")
+const filterBtns = document.querySelectorAll(".filter-btns button")
+const filterCards = document.querySelectorAll(".filter-cards .card")
+
+
+const filterableCards = e => {
+    document.querySelector(".active").classList.remove(".active");
+    e.target.classList.add(".active");
+  
+    filterCards.forEach(card => {
+        card.classList.add("hide");
+        if (card.dataset.name === e.target.dataset.name || e.target.dataset.name === 'all'){
+            card.classList.remove("hide")
+        }
+    })
+    console.log(e.target)
+}
+filterBtns.forEach(button => button.addEventListener("click", filterableCards))
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+const filterTextBtns = document.querySelectorAll(".fluid button")
+const filterParra = document.querySelectorAll(".text .carta")
+
+const filterFunc = e => {
+    document.querySelector(".active").classList.remove(".active");
+    e.target.classList.add(".active");
+    console.log(e.target)
+
+    filterParra.forEach(carta => {
+        carta.classList.add("hide");
+        if(carta.dataset.text === e.target.dataset.text || e.target.dataset.text === 'all'){
+            carta.classList.remove("hide")
+        }
+
+    })
+}
+
+filterTextBtns.forEach(button => button.addEventListener("click", filterFunc))
+
+
+
